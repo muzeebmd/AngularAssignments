4dbc4db591f24b59ba2bbaadbb85e52a3d3c68d2
muzeebmd <143504379+muzeebmd@users.noreply.github.com>
2025-04-07 16:39:22 +0530

    Angular-Components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,12 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Simple-form';
-  // Properties for data binding
-  userName: string = '';
-  userCity: string = '';
-  userEmail: string = '';
-
-  // Method to handle button click event
-  updateUser() {
-    alert(`User Updated: ${this.userName}, ${this.userCity}, ${this.userEmail}`);
-  }
+  title = 'Components';
 }
