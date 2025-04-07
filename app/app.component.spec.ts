c0601b043f0dbc3c23004f440d9545d75af3cce9
muzeebmd <143504379+muzeebmd@users.noreply.github.com>
2025-04-07 16:32:06 +0530

    Data-Binding

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Components' title`, () => {
+  it(`should have the 'Simple-form' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Components');
+    expect(app.title).toEqual('Simple-form');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Components');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Simple-form');
   });
 });
--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,26 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { AwardsComponent } from './awards/awards.component';
-import { StatsComponent } from './stats/stats.component';
 
-export const routes: Routes = [
-
-    {
-        path: 'home',
-        component: HomeComponent,
-
-    },
-    {
-        path: 'about',
-        component: AboutComponent
-    },
-    {
-        path: 'awards',
-        component: AwardsComponent
-    },
-    {
-        path: 'stats',
-        component: StatsComponent,
-    }
-];
+export const routes: Routes = [];
